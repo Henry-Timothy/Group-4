2d282a73847bdb7f9ed7e18571c2b721e488fa12
farrelbas <aliffianarosa@gmail.com>
2024-05-04 20:29:20 +0700

    data tabel

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -31,7 +31,25 @@ class DashboardController extends Controller
                 'user' => $this->get_id_user($request),
             ];
 
+            $data = DB::table('tb_item')
+                ->join('tb_detail_transaction', 'tb_detail_transaction.id_item', '=', 'tb_item.id_item')
+                ->join('tb_purchase', 'tb_purchase.id_item', '=', 'tb_detail_transaction.id_item')
+                ->select(
+                    'tb_item.id_item',
+                    'tb_item.item_name',
+                    DB::raw('SUM(tb_detail_transaction.qty) as selling_qty'),
+                    DB::raw('(SELECT price FROM tb_detail_transaction WHERE tb_detail_transaction.id_item = tb_item.id_item LIMIT 1) as selling_price'),
+                    DB::raw('SUM(tb_detail_transaction.qty * tb_detail_transaction.price) as total_selling_price'),
+                    DB::raw('(SELECT REPLACE(REPLACE(purchase_price, ".", ""), ",", ".") / purchase_amount FROM tb_purchase WHERE tb_purchase.id_item = tb_item.id_item LIMIT 1) * SUM(tb_detail_transaction.qty) as total_purchase_price'),
+                    DB::raw('(SUM(tb_detail_transaction.qty * tb_detail_transaction.price) - (SELECT REPLACE(REPLACE(purchase_price, ".", ""), ",", ".") / purchase_amount FROM tb_purchase WHERE tb_purchase.id_item = tb_item.id_item LIMIT 1) * SUM(tb_detail_transaction.qty)) as calculate')
+                )
+                ->where('tb_detail_transaction.id_transaction', '!=', 0)
+                ->where('detail_transaction_softdel', 0)
+                ->groupBy('tb_item.id_item', 'tb_item.item_name')
+                ->get();
+
             return view('Dashboard.dashboard', $data, [
+                'data' => $data,
                 'title' => 'Dashboard',
             ]);
         } else {
